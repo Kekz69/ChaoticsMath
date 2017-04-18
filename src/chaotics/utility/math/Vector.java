ba01bf06ac15e3a452e736aa612430afd9327cf8
Hans Dieter <realVK@gmx.de>
2017-04-19 00:16:29 +0200

    math - vector   -> more set methods

--- src/chaotics/utility/math/Vector.java
+++ src/chaotics/utility/math/Vector.java
@@ -4,13 +4,20 @@ package chaotics.utility.math;
  * Created by Hans on 18.04.2017.
  */
 public class Vector {
+    private int dim;
     private double[] vectorArray;
 
     public Vector(int dim) {
-        vectorArray = new double[dim > 1 ? dim : 0];
+        this.dim = dim;
+        vectorArray = new double[dim > 1 ? dim : 2];
     }
 
     public void set(int index, double value) {
         vectorArray[index] = value;
+    }
+
+    public void set(double[] newVectorArray) {
+        vectorArray = newVectorArray;
+        dim = vectorArray.length;
     }
 }
